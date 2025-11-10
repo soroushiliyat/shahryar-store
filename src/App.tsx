e9ebfd93cccacfec3c9825437261ad31057139c1
Soroush Iliyat <soroushiliyat23@gmail.com>
2025-11-10 17:25:14 +0330

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,6 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
           </Routes>
         </div>
 
